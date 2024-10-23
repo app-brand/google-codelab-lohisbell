f7db5baa909e5d4f4817f379e7d5e1871b639b21
Carlos <data4if@gmail.com>
2024-10-23 08:42:02 -0400

    running - automatics - update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:codelab_training/presentation/core/home_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const HomeCore());
 }
